9658da4a004298cea6ae511378966ec27994c2c2
tfraley7 <tyfraley81@gmail.com>
2022-11-23 15:05:39 -0500

    Update

--- Static/charts.js
+++ Static/charts.js
@@ -3,7 +3,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
+  d3.json("../Data/samples.json").then((data) => {
     console.log(data);
     var sampleNames = data.names;
     sampleNames.forEach((sample) => {
@@ -13,11 +13,12 @@ function init() {
         .property("value", sample);
     });
     const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-})};
+    buildCharts(firstSample)
+    buildMetadata(firstSample)
+  })
+};
 //Initialize the dashboard
-init();
+init()
 
 // 1. Create the buildCharts function.
 function optionChanged(newSample) {
